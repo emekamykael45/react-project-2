70fbebc0ff69ee4f0d8c57269fc054aeda7ef498
MYKEL DOMINIC PC <emekamykael45@gmail.com>
2019-04-17 12:07:25 +0100

    Updates

---
+++ precache-manifest.40232ed12a497b6733aea39311f9310c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d832009feafd99945b5d",
+    "url": "/react-project-2/static/js/runtime~main.2b62bd7f.js"
+  },
+  {
+    "revision": "59a2baeec240c468af67",
+    "url": "/react-project-2/static/js/main.9780c871.chunk.js"
+  },
+  {
+    "revision": "b9572d93d49208d17f63",
+    "url": "/react-project-2/static/js/2.1cfc7d0f.chunk.js"
+  },
+  {
+    "revision": "59a2baeec240c468af67",
+    "url": "/react-project-2/static/css/main.bcaa50a6.chunk.css"
+  },
+  {
+    "revision": "08defc5f2e5eb4a8c67eed6d97c11a97",
+    "url": "/react-project-2/index.html"
+  }
+];
